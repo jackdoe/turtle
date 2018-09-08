2f039729e3dc60a390f7ef92b8a41cb133a3e7c7
Borislav Nikolov <borislav.nikolov@booking.com>
2018-09-08 10:28:46 +0200

    add support for -q ::

--- src/main/java/bz/turtle/readable/ReadableModel.java
+++ src/main/java/bz/turtle/readable/ReadableModel.java
@@ -26,6 +26,7 @@ public class ReadableModel {
   // -q ab
   // -q ac
   public Map<Character, Set<Character>> quadratic = new HashMap<>();
+  public boolean quadraticAnyToAny = false;
 
   @Override
   public String toString() {
@@ -88,7 +89,7 @@ public class ReadableModel {
             }
             if (x.contains("options")) {
               extractOptions(
-                  x.split(":")[1],
+                  x.split(":", 2)[1],
                   (key, value) -> {
                     if (key.equals("--oaa")) {
                       maxLabels = Integer.parseInt(value);
@@ -106,9 +107,13 @@ public class ReadableModel {
                     }
 
                     if (key.equals("--quadratic")) {
-                      quadratic
-                          .computeIfAbsent(value.charAt(0), k -> new HashSet<>())
-                          .add(value.charAt(1));
+                      if (value.equals("::")) {
+                        quadraticAnyToAny = true;
+                      } else {
+                        quadratic
+                            .computeIfAbsent(value.charAt(0), k -> new HashSet<>())
+                            .add(value.charAt(1));
+                      }
                     }
                     // TODO: --cubic
                     // TODO: ngrams, skips
@@ -217,46 +222,68 @@ public class ReadableModel {
               });
         });
 
-    if (quadratic.size() > 0) {
-      Map<Character, List<Namespace>> prebuild = new HashMap<>();
-
-      // build char -> list of namespaces map so we can work with multiple interactions -q ab -q ac
-      input.namespaces.forEach(
-          n -> {
-            if (n.namespace.length() == 0) return;
-            prebuild.computeIfAbsent(n.namespace.charAt(0), k -> new ArrayList<>()).add(n);
-          });
+    if (quadratic.size() > 0 || quadraticAnyToAny) {
 
       // foreach namespace nsA
       //    foreach interacting namespaces nsB
       //       foreach nsA.features a
       //         foreach nsB.feature b
       //            bucket = ((a._computed_hash * FNV_prime) ^ b._computed_hash);
-      input.namespaces.forEach(
-          ans -> {
-            Set<Character> interactStartingWith = quadratic.get(ans.namespace.charAt(0));
-            if (interactStartingWith == null) return;
-            interactStartingWith.forEach(
-                inter -> {
-                  List<Namespace> interactions = prebuild.get(inter);
-                  if (interactions == null) return;
-                  interactions.forEach(
-                      bns -> {
-                        ans.features.forEach(
-                            a -> {
-                              bns.features.forEach(
-                                  b -> {
-                                    int fnv = ((a._computed_hash * FNV_prime) ^ b._computed_hash);
-                                    for (int klass = 0; klass < maxLabels; klass++) {
-                                      int bucket = getBucket(fnv, klass);
-                                      // TODO: check how is that computed for numerical features
-                                      out[klass] += a.value * b.value * weights[bucket];
-                                    }
-                                  });
-                            });
-                      });
-                });
-          });
+
+      if (!quadraticAnyToAny) {
+        input.namespaces.forEach(
+            ans -> {
+              input.namespaces.forEach(
+                  bns -> {
+                    if (ans.namespace.equals(bns.namespace)) return;
+                    ans.features.forEach(
+                        a -> {
+                          bns.features.forEach(
+                              b -> {
+                                int fnv = ((a._computed_hash * FNV_prime) ^ b._computed_hash);
+                                for (int klass = 0; klass < maxLabels; klass++) {
+                                  int bucket = getBucket(fnv, klass);
+                                  out[klass] += a.value * b.value * weights[bucket];
+                                }
+                              });
+                        });
+                  });
+            });
+      } else {
+        Map<Character, List<Namespace>> prebuild = new HashMap<>();
+        // build char -> list of namespaces map so we can work with multiple interactions -q ab -q
+        // ac
+        input.namespaces.forEach(
+            n -> {
+              if (n.namespace.length() == 0) return;
+              prebuild.computeIfAbsent(n.namespace.charAt(0), k -> new ArrayList<>()).add(n);
+            });
+        input.namespaces.forEach(
+            ans -> {
+              Set<Character> interactStartingWith = quadratic.get(ans.namespace.charAt(0));
+              if (interactStartingWith == null) return;
+              interactStartingWith.forEach(
+                  inter -> {
+                    List<Namespace> interactions = prebuild.get(inter);
+                    if (interactions == null) return;
+                    interactions.forEach(
+                        bns -> {
+                          ans.features.forEach(
+                              a -> {
+                                bns.features.forEach(
+                                    b -> {
+                                      int fnv = ((a._computed_hash * FNV_prime) ^ b._computed_hash);
+                                      for (int klass = 0; klass < maxLabels; klass++) {
+                                        int bucket = getBucket(fnv, klass);
+                                        // TODO: check how is that computed for numerical features
+                                        out[klass] += a.value * b.value * weights[bucket];
+                                      }
+                                    });
+                              });
+                        });
+                  });
+            });
+      }
     }
 
     if (input.hasIntercept) {
