f9d4a62e0a954e22aff1fd2eacf4909a6ca0b71f
Borislav Nikolov <borislav.nikolov@booking.com>
2018-09-09 10:13:30 +0200

    create a testset and parallelize the tests

--- src/test/java/bz/turtle/readable/Fuzz.java
+++ src/test/java/bz/turtle/readable/Fuzz.java
@@ -10,53 +10,10 @@ import java.util.Comparator;
 import java.util.Random;
 
 public class Fuzz {
-  public void cleanup(File path) throws IOException {
-    Path pathToBeDeleted = Paths.get(path.toURI());
-
-    Files.walk(pathToBeDeleted)
-        .sorted(Comparator.reverseOrder())
-        .map(Path::toFile)
-        .forEach(File::delete);
-  }
 
   public static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-  Random r = new Random();
-
-  public String createExample(int klass) {
-    StringBuilder sb = new StringBuilder();
-    sb.append(klass);
-    sb.append(" ");
-    for (int nsA = 0; nsA < 15; nsA++) {
-      for (int nsB = 0; nsB < 2; nsB++) {
-        String ns =
-            Character.toString(alphabet.charAt(nsA)) + Character.toString(alphabet.charAt(nsB));
-        sb.append("|");
-        sb.append(ns);
-        sb.append(" ");
-        for (int i = 1; i < 10; i++) {
-          sb.append("n");
-          sb.append((char) (r.nextInt(26) + 'a'));
-          sb.append(":");
-          sb.append(i);
-          sb.append(" ");
-        }
-
-        for (int i = 0; i < 10; i++) {
-          sb.append("c");
-          sb.append((char) (r.nextInt(26) + 'a'));
-          sb.append("=");
-          sb.append(i);
-          sb.append(" ");
-        }
-      }
-    }
-    sb.append("\n");
-    return sb.toString();
-  }
-
   public void runOrExit(String cmd) throws Exception {
-
     Process p = Runtime.getRuntime().exec(cmd);
 
     BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -80,57 +37,118 @@ public class Fuzz {
     }
   }
 
-  public void runVW(String optionsTrain, String optionsTest) throws Exception {
+  public void runVW(TestSet ts, String optionsTrain, String optionsTest) throws Exception {
     runOrExit(
         String.format(
             "vw -d %s --min_prediction -10000 --max_prediction 10000 --readable_model %s -f %s %s",
-            data, model, modelBin, optionsTrain));
-    runOrExit(String.format("vw -d %s -t -i %s -r %s %s", data, modelBin, pred, optionsTest));
-    ReadableModel m = new ReadableModel(tempDir);
-    m.makeSureItWorks(data, pred);
+            ts.data, ts.model, ts.modelBin, optionsTrain));
+    runOrExit(
+        String.format("vw -d %s -t -i %s -r %s %s", ts.data, ts.modelBin, ts.pred, optionsTest));
+    ReadableModel m = new ReadableModel(ts.tempDir);
+    m.makeSureItWorks(ts.data, ts.pred);
   }
 
   @Test
-  public void testMany() throws Exception {
+  public void testOaa() throws Exception {
     if (vwfound()) {
-      setUp();
-      runVW("--oaa 10", "");
-
-      runVW("", "");
-      runVW("-q ab", "");
-      runVW("-q ab -q cd -q ac -q bc", "");
-
-      // XXX: accumulates error with many namespaces
-      //    runVW("-q ::", "");
-      tearDown();
+      TestSet ts = new TestSet(10, 1000);
+      runVW(ts, "--oaa 10", "");
+      ts.tearDown();
     }
   }
 
-  File tempDir, data, pred, model, modelBin;
+  @Test
+  public void testBasic() throws Exception {
+    if (vwfound()) {
+      TestSet ts = new TestSet(1, 10000);
+      runVW(ts, "", "");
+      ts.tearDown();
+    }
+  }
+
+  @Test
+  public void testQuadratic() throws Exception {
+    if (vwfound()) {
+      TestSet ts = new TestSet(3, 1000);
+
+      runVW(ts, "--oaa 3 -q ab", "");
+      runVW(ts, "--oaa 3 -q ab -q cd -q ac -q bc", "");
+      ts.tearDown();
+    }
+  }
+
+  public static class TestSet {
+    File tempDir, data, pred, model, modelBin;
+    Random r = new Random();
+
+    public String createExample(int klass) {
+      StringBuilder sb = new StringBuilder();
+      sb.append(klass);
+      sb.append(" ");
+      for (int nsA = 0; nsA < 5; nsA++) {
+        String ns = Character.toString(alphabet.charAt(nsA));
+        sb.append("|");
+        sb.append(ns);
+        sb.append(" ");
+        for (int i = 1; i < 10; i++) {
+          sb.append("n");
+          sb.append((char) (r.nextInt(26) + 'a'));
+          sb.append(":");
+          sb.append(i);
+          sb.append(" ");
+        }
+
+        for (int i = 0; i < 10; i++) {
+          sb.append("c");
+          sb.append((char) (r.nextInt(26) + 'a'));
+          sb.append("=");
+          sb.append(i);
+          sb.append(" ");
+        }
+      }
+      sb.append("\n");
+      return sb.toString();
+    }
+
+    public TestSet(int nClassess, int nExamples) throws Exception {
+      this.tempDir = Files.createTempDirectory("foobar").toFile();
+      this.data = Paths.get(this.tempDir.toString(), "test.txt").toFile();
+      this.pred = Paths.get(this.tempDir.toString(), "predictions.txt").toFile();
+      System.out.println(this.tempDir);
+      this.model = Paths.get(this.tempDir.toString(), "readable_model.txt").toFile();
+      this.modelBin = Paths.get(this.tempDir.toString(), "model.bin").toFile();
+      FileWriter fw = new FileWriter(this.data);
+      BufferedWriter writer = new BufferedWriter(fw);
+
+      for (int i = 0; i < nExamples; i++) {
+        if (nClassess == 0) {
+          writer.write(createExample(1));
+          writer.write(createExample(-1));
+        } else {
+          for (int klass = 1; klass < nClassess; klass++) {
+            writer.write(createExample(klass));
+          }
+        }
+      }
+      writer.close();
+      fw.close();
+    }
+
+    public void cleanup(File path) throws IOException {
+      Path pathToBeDeleted = Paths.get(path.toURI());
+
+      Files.walk(pathToBeDeleted)
+          .sorted(Comparator.reverseOrder())
+          .map(Path::toFile)
+          .forEach(File::delete);
+    }
+
+    public void tearDown() throws Exception {
+      cleanup(this.tempDir);
+    }
+  }
 
   public boolean vwfound() {
-    return true;
-    //    return new File("/usr/local/bin/vw").exists() || new File("/usr/bin/vw").exists();
-  }
-
-  public void setUp() throws Exception {
-    tempDir = Files.createTempDirectory("foobar").toFile();
-    data = Paths.get(tempDir.toString(), "test.txt").toFile();
-    pred = Paths.get(tempDir.toString(), "predictions.txt").toFile();
-    System.out.println(tempDir);
-    model = Paths.get(tempDir.toString(), "readable_model.txt").toFile();
-    modelBin = Paths.get(tempDir.toString(), "model.bin").toFile();
-    BufferedWriter writer = new BufferedWriter(new FileWriter(data));
-
-    for (int i = 0; i < 100; i++) {
-      for (int klass = 1; klass < 10; klass++) {
-        writer.write(createExample(klass));
-      }
-    }
-    writer.close();
-  }
-
-  public void tearDown() throws Exception {
-    cleanup(tempDir);
+    return new File("/usr/local/bin/vw").exists() || new File("/usr/bin/vw").exists();
   }
 }
